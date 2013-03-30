775df01befce0cd70926737894de5a974a20cd94
John Gietzen <otac0n@gietzen.us>
2013-03-30 15:08:56 -0700

    Created an optimized version of the cocktail sort.

--- sorts/CocktailSort.js
+++ sorts/CocktailSort.js
@@ -49,3 +49,38 @@ function FastCocktailSort(values) {
     right.destroy();
     i.destroy();
 }
+
+function OptimizedCocktailSort(values) {
+    var i = values.pointer("i", 0);
+    var left = values.pointer("left", 0);
+    var right = values.pointer("right", values.length - 2);
+    var start = values.pointer("start", 0);
+    var swapped;
+    do {
+        swapped = 0;
+        for (i.set(start.value); i.value <= right.value; i.add(1)) {
+            if (values.compare(i, i.value + 1) > 0) {
+                values.swap(i, i.value + 1);
+                start.set(i.value - 1);
+                swapped++;
+            }
+        }
+        right.sub(1);
+        if (swapped == 0) {
+            break;
+        }
+        swapped--;
+        for (i.set(start.value); i.value >= left.value; i.sub(1)) {
+            if (values.compare(i, i.value + 1) > 0) {
+                values.swap(i, i.value + 1);
+                start.set(i.value + 1);
+                swapped++;
+            }
+        }
+        left.add(1);
+    } while (swapped > 0);
+    start.destroy();
+    left.destroy();
+    right.destroy();
+    i.destroy();
+}
