ad6f12769e1c1c83c0d77095538dc57fe9517b21
John Gietzen <otac0n@gietzen.us>
2013-03-31 18:48:45 -0700

    Scope everything in the bar renderer to the parent div.

--- renderers/Bar.js
+++ renderers/Bar.js
@@ -1,6 +1,6 @@
 function RenderBar(operations, sync, target) {
     var frame = 0;
-    $parent = $('<div class="bar" />').appendTo(target);
+    var $parent = $('<div class="bar" />').appendTo(target);
 
     function getHeight(value) {
         return (+value + 1) * 2 + 'px';
@@ -23,25 +23,25 @@ function RenderBar(operations, sync, target) {
 
         switch (op.op) {
             case "array.create":
-                div = $('<div class="array" />').attr('id', 'array' + op.id).appendTo($parent)[0];
+                $div = $('<div class="array" />').addClass('array' + op.id).appendTo($parent);
                 for (var i = 0; i < op.length; i++) {
-                    $('<div class="element" />').css({ height: getHeight(op.value ? op.value[i] : 0) }).data('value', op.value ? op.value[i] : null).appendTo(div);
+                    $('<div class="element" />').css({ height: getHeight(op.value ? op.value[i] : 0) }).data('value', op.value ? op.value[i] : null).appendTo($div);
                 }
                 break;
             case "array.set":
-                $("#array" + op.id).find('.element').eq(op.index).css({ height: getHeight(op.value) }).data('value', op.value);
+                $parent.find(".array" + op.id).find('.element').eq(op.index).css({ height: getHeight(op.value) }).data('value', op.value);
                 break;
             case "array.destroy":
-                $("#array" + op.id).remove();
+                $parent.find(".array" + op.id).remove();
                 break;
             case "array.compare":
-                $("#array" + op.idA).find('.element').eq(op.indexA).css({ backgroundColor: '#fff' })
+                $parent.find(".array" + op.idA).find('.element').eq(op.indexA).css({ backgroundColor: '#fff' })
                     .animate({ backgroundColor: '#ff0' }, 400/sync.speed, function () {
                         $(this).animate({ backgroundColor: '#fff' }, 400/sync.speed, function () {
                             animationFinish();
                         });
                     });
-                $("#array" + op.idB).find('.element').eq(op.indexB).css({ backgroundColor: '#fff' })
+                $parent.find(".array" + op.idB).find('.element').eq(op.indexB).css({ backgroundColor: '#fff' })
                     .animate({ backgroundColor: '#ff0' }, 400/sync.speed, function () {
                         $(this).animate({ backgroundColor: '#fff' }, 400/sync.speed, function () {
                             animationFinish();
@@ -50,8 +50,8 @@ function RenderBar(operations, sync, target) {
                 animations = 2;
                 break;
             case "array.swap":
-                var $a = $("#array" + op.idA).find('.element').eq(op.indexA);
-                var $b = $("#array" + op.idB).find('.element').eq(op.indexB);
+                var $a = $parent.find(".array" + op.idA).find('.element').eq(op.indexA);
+                var $b = $parent.find(".array" + op.idB).find('.element').eq(op.indexB);
                 var a = $b.data('value');
                 var b = $a.data('value');
  
@@ -60,13 +60,13 @@ function RenderBar(operations, sync, target) {
 
                 break;
             case "pointer.create":
-                $('<i />').addClass('pointer').attr('id', 'pointer' + op.id).text(op.name[0]).appendTo($('#array' + op.array).find('.element').eq(op.value));
+                $('<i class="pointer" />').addClass('pointer' + op.id).text(op.name[0]).appendTo($parent.find(".array" + op.array).find('.element').eq(op.value));
                 break;
             case "pointer.set":
-                $('#pointer' + op.id).appendTo($('#pointer' + op.id).parents('.array').find('.element').eq(op.value));
+                $parent.find(".pointer" + op.id).appendTo($parent.find(".pointer" + op.id).parents('.array').find('.element').eq(op.value));
                 break;
             case "pointer.destroy":
-                $('#pointer' + op.id).remove();
+                $parent.find(".pointer" + op.id).remove();
                 break;
         }
 
