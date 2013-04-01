ad54bee8b9af4e59e9c39aed83ed3eae69ea9b24
John Gietzen <otac0n@gietzen.us>
2013-03-31 20:00:07 -0700

    Scope everything in the simple renderer to the parent div.

--- renderers/Simple.js
+++ renderers/Simple.js
@@ -1,6 +1,6 @@
 function RenderSimple(operations, sync, target) {
     var frame = 0;
-    $parent = $('<div class="simple" />').appendTo(target);
+    var $parent = $('<div class="simple" />').appendTo(target);
 
     var renderOperation = function () {
         if (sync.stop || frame >= operations.length) {
@@ -19,26 +19,26 @@ function RenderSimple(operations, sync, target) {
 
         switch (op.op) {
             case "array.create":
-                div = $('<div class="array" />').attr('id', 'array' + op.id).appendTo($parent)[0];
+                $div = $('<div class="array" />').addClass('array' + op.id).appendTo($parent);
                 for (var i = 0; i < op.length; i++) {
-                    $element = $('<div class="element" />').append($('<span class="value" />')).appendTo(div);
+                    $element = $('<div class="element" />').append($('<span class="value" />')).appendTo($div);
                     $element.find('.value').text(op.value ? op.value[i] : "\xB7");
                 }
                 break;
             case "array.set":
-                $("#array" + op.id).find('.element').eq(op.index).find('.value').text(op.value === null ? "\xB7" : op.value);
+                $parent.find('.array' + op.id).find('.element').eq(op.index).find('.value').text(op.value === null ? "\xB7" : op.value);
                 break;
             case "array.destroy":
-                $("#array" + op.id).remove();
+                $parent.find('.array' + op.id).remove();
                 break;
             case "array.compare":
-                $("#array" + op.idA).find('.element').eq(op.indexA).css({ backgroundColor: '#fff' })
+                $parent.find('.array' + op.idA).find('.element').eq(op.indexA).css({ backgroundColor: '#fff' })
                     .animate({ backgroundColor: '#ff0' }, 400/sync.speed, function () {
                         $(this).animate({ backgroundColor: '#fff' }, 400/sync.speed, function () {
                             animationFinish();
                         });
                     });
-                $("#array" + op.idB).find('.element').eq(op.indexB).css({ backgroundColor: '#fff' })
+                $parent.find('.array' + op.idB).find('.element').eq(op.indexB).css({ backgroundColor: '#fff' })
                     .animate({ backgroundColor: '#ff0' }, 400/sync.speed, function () {
                         $(this).animate({ backgroundColor: '#fff' }, 400/sync.speed, function () {
                             animationFinish();
@@ -47,41 +47,23 @@ function RenderSimple(operations, sync, target) {
                 animations = 2;
                 break;
             case "array.swap":
-                var $a = $("#array" + op.idA).find('.element').eq(op.indexA).find('.value');
-                var $b = $("#array" + op.idB).find('.element').eq(op.indexB).find('.value');
+                var $a = $parent.find('.array' + op.idA).find('.element').eq(op.indexA).find('.value');
+                var $b = $parent.find('.array' + op.idB).find('.element').eq(op.indexB).find('.value');
                 var a = $b.text();
                 var b = $a.text();
 
-                if (false) {
-                    $a.css({ color: '#000' })
-                        .animate({ color: '#fff' }, 400/sync.speed, function () {
-                            $a.text(a);
-                            $(this).animate({ color: '#000' }, 400/sync.speed, function () {
-                                animationFinish();
-                            });
-                        });
-                    $b.css({ color: '#000' })
-                        .animate({ color: '#fff' }, 400/sync.speed, function () {
-                            $b.text(b);
-                            $(this).animate({ color: '#000' }, 400/sync.speed, function () {
-                                animationFinish();
-                            });
-                        });
-                    animations = 2;
-                } else {
-                    $a.text(a);
-                    $b.text(b);
-                }
+                $a.text(a);
+                $b.text(b);
 
                 break;
             case "pointer.create":
-                $('<i />').addClass('pointer').attr('id', 'pointer' + op.id).text(op.name).appendTo($('#array' + op.array).find('.element').eq(op.value));
+                $('<i class="pointer" />').addClass('pointer' + op.id).text(op.name).appendTo($parent.find('.array' + op.array).find('.element').eq(op.value));
                 break;
             case "pointer.set":
-                $('#pointer' + op.id).appendTo($('#pointer' + op.id).parents('.array').find('.element').eq(op.value));
+                $parent.find('.pointer' + op.id).appendTo($parent.find('.pointer' + op.id).parents('.array').find('.element').eq(op.value));
                 break;
             case "pointer.destroy":
-                $('#pointer' + op.id).remove();
+                $parent.find('.pointer' + op.id).remove();
                 break;
         }
 
