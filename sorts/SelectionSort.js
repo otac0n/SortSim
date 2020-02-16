3d0a0f590b969ad18aff28f2f9e453215e4f7cc8
John Gietzen <john@gietzen.us>
2020-02-16 13:03:18 -0800

    Added the Double selection sort algorithm.

--- sorts/SelectionSort.js
+++ sorts/SelectionSort.js
@@ -1,6 +1,6 @@
 function SelectionSort(values) {
     var i = values.pointer("i");
-    var j = values.pointer("j");
+    var j = values.pointer("target");
     var min = values.pointer("min");
     for (j.set(0); j.value < values.length - 1; j.add(1)) {
         min.set(j);
@@ -20,6 +20,45 @@ function SelectionSort(values) {
     min.destroy();
 }
 
+function DoubleSelectionSort(values) {
+    var i = values.pointer("i");
+    var l = values.pointer("left");
+    var r = values.pointer("right");
+    var min = values.pointer("min");
+    var max = values.pointer("max");
+    for (l.set(0), r.set(values.length - 1); l.value < Math.floor(values.length / 2); l.add(1), r.add(-1)) {
+        min.set(l);
+        max.set(l);
+
+        for (i.set(l.value + 1); i.value <= r.value; i.add(1)) {
+            if (values.compare(i, min) < 0) {
+                min.set(i);
+            }
+            else if (values.compare(i, max) >= 0) {
+                max.set(i);
+            }
+        }
+
+        if (min.value != l.value) {
+            values.swap(l, min);
+
+            if (max.value == l.value) {
+                max.set(min.value);
+            }
+        }
+
+        if (max.value != r.value) {
+            values.swap(r, max);
+        }
+    }
+    i.destroy();
+    l.destroy();
+    r.destroy();
+    min.destroy();
+    max.destroy();
+}
+
 if (SortAlgorithms) {
     SortAlgorithms["SelectionSort"] = { name: "Selection sort", sort: SelectionSort };
+    SortAlgorithms["DoubleSelectionSort"] = { name: "Double selection sort", sort: DoubleSelectionSort };
 }
