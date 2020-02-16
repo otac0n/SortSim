d3e6ea6b4ecc58dc5b6baea12b7b68e16c0649af
John Gietzen <john@gietzen.us>
2020-02-16 13:04:11 -0800

    Added emojis for the Gnome sort algorithm.

--- sorts/GnomeSort.js
+++ sorts/GnomeSort.js
@@ -1,5 +1,30 @@
 function GnomeSort(values) {
-    var i = values.pointer("i", 1);
+    var emojis = [
+        // Robot Face
+        //"\uD83E\uDD16",
+
+        // Monkey Face
+        "\uD83D\uDC35",
+
+        // Mouse
+        "\uD83D\uDC01",
+
+        // Chipmunk
+        //"\uD83D\uDC3F",
+
+        // Left Pointing Hand
+        "\uD83D\uDC48",
+
+        // Elves
+        "\uD83E\uDDDD",
+        "\uD83E\uDDDD\uD83C\uDFFB",
+        "\uD83E\uDDDD\uD83C\uDFFC",
+        "\uD83E\uDDDD\uD83C\uDFFD",
+        "\uD83E\uDDDD\uD83C\uDFFE",
+        "\uD83E\uDDDD\uD83C\uDFFF",
+    ];
+
+    var i = values.pointer(emojis[Math.random() * emojis.length | 0], 1);
     while (i.value < values.length) {
         if (values.compare(i, i.value - 1) >= 0) {
             i.add(1);
